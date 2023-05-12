3da187b75179ee378fa3e46a37d4e5ceb437b463
huyenlt <huyen.lt@cerebral.com>
2023-05-12 12:57:46 +0700

    5 Commit

--- testNGBatchFile.bat
+++ testNGBatchFile.bat
@@ -1,5 +1,1 @@
-set projectLocation=D:\Work\AT19_MavenProject
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
+mvn clean test -DsuiteXmlFile=testng.xml
