de9699a223bbf0420fdcac3eaea3caa8efd81261
huyenlt <huyen.lt@cerebral.com>
2023-05-12 11:23:52 +0700

    Third Commit

--- testNGBatchFile.bat
+++ testNGBatchFile.bat
@@ -1,5 +1,5 @@
 set projectLocation=D:\Work\AT19_MavenProject
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
+java org.testng.TestNG testng.xml
 pause
